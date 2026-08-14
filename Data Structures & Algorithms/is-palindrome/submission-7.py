a1d30426ef82dd2364bf96c9be7356227730e244
Nathan Zake <nathanwzake@gmail.com>
2026-08-14 20:02:29 +0300

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z')) or (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9'))
+
+    def isPalindrome(self, s: str) -> bool:
+
+        L, R = 0, len(s) - 1
+
+        while L < R:
+            while not self.alphaNum(s[L]) and L < R:
+                L += 1
+
+            while not self.alphaNum(s[R]) and L < R:
+                R -= 1
+            
+            if s[L].lower() != s[R].lower():
+                return False
+            
+            L += 1
+            R -= 1
+        
+        return True
+        
